3b723553142242f4813376e1ea8ec0c22e5ffecd
gowthaman <ngowthaman19@gmail.com>
2025-10-10 11:52:45 +0530

    removed gowtham folder

--- linux/gowtham/script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-read -p "enter the file name: " file_name
-
-echo "no of line in the file: `wc -l $file_name`"
-echo "no of word in the file: `wc -w $file_name`"
-echo "no of char in the file: `wc -c $file_name`"
