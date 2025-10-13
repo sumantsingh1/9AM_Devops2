0c685e1c64fb7f87905644348e33a971b79d6284
syedafzal2902 <syedafzal2902@gmail.com>
2025-10-13 12:35:03 +0530

    Deleted RE-Accurence Files

--- test2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-file="my_file"
-
-echo "the No of lines in my file was:`wc -l $file1`"
-echo "the No of words in my file was:`wc -w $file`"
-echo "the No of charecters in my file was :`wc -c $file`"
