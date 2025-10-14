2e6196986ff56f45cb3795dda6a436bc1bcdb563
Pooja614-cell <poojapatilj99@gmail.com>
2025-10-14 14:50:59 +0530

    modified file

--- linux/task11.sh
+++ linux/task11.sh
@@ -6,7 +6,7 @@ echo "-------------------------------------------"
 echo ""
 
 # Display top 10 processes sorted by CPU usage
-ps -eo pid,ppid,cmd,%mem,%cpu --sort=-%cpu | head -n 11
+ps aux --sort=-%cpu | head -n 11
 
 echo ""
 echo "-------------------------------------------"
---
+++ linux/task16.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+read -p "Enter your username: " username
+
+#Checking username entered is empty
+if [ -z "$username" ]; then 
+
+	echo "Username can't be empty "
+	exit 1
+fi
+
+#Checking for username contains only letters and uppercase
+
+if [ [ "$username" =~ ^[A-Z]+$ ] ]; then
+    
+	echo "Valid username: $username "
+
+else 
+	echo "invalid username , it must be uppercase letters only"
+fi
+
+
